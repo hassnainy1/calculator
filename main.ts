2c6f0d5d2aaba6a90d8acd38c904080c533c274e
hassnainy1 <138851184+hassnainy1@users.noreply.github.com>
2023-08-26 13:26:26 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,49 @@
+
+
+import inquirer from "inquirer";
+
+const answers :{
+  num1:number
+  num2:number
+  operator :string
+} = await inquirer.prompt([
+  {
+    name: "num1",
+    message: "kindly enter first digit ",
+    type: "number",
+  },
+  {
+    name: "num2",
+    message: "kindly enter second digit",
+    type: "number",
+  },
+  {
+    type : "list",
+    name:"operator",
+      choices :["addition","subtraction","multiplication","division"],
+    message:"select operator",
+  },
+]);
+
+const { num1 , num2, operator} = answers;
+if(num1 && num2 && operator){
+let result: number = 0
+if(operator ==="subtraction"){
+  result = num1 - num2
+}
+else if(operator ==="addition"){
+  result = num1 + num2
+}
+else if(operator ==="division"){
+  result = num1 / num2
+}
+else if(operator ==="multiplication"){
+  result = num1 * num2
+}
+
+console.log ("your answer is :",result)
+
+
+}else{
+  console.log("kindly enter valid  number")
+}
